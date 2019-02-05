f38a278bd877aa4023481953be0d7842ae011ce0
Mariam Jaludi <mariam.jaludi@gmail.com>
2019-02-05 14:56:17 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -15,4 +15,33 @@ def pop_with_args(array)
 end
 
 def using_shift(array)
-  
+  return array.shift
+end
+
+def shift_with_args(array)
+  return array.shift(2)
+end
+
+def using_concat(array1, array2)
+
+end
+
+def using_insert(array, element)
+
+end
+
+def using_uniq(array)
+
+end
+
+def using_flatten(array)
+
+end
+
+def using_delete(array)
+
+end
+
+def using_delete_at(array)
+
+end
