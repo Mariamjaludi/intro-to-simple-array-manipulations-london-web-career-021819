676d12e936622c452dac5f331e976422835856d4
Mariam Jaludi <mariam.jaludi@gmail.com>
2019-02-05 14:51:18 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,18 @@
+def using_push(array, string)
+  array.push(string)
+end
+
+def using_unshift(array, string)
+  array.unshift(string)
+end
+
+def using_pop(array)
+  return array.pop
+end
+
+def pop_with_args(array)
+  return array.pop(2)
+end
+
+def using_shift(array)
+  
